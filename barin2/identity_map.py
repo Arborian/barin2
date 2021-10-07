cfe9fbc05778a4bc1f395b159d70bb2e4c459917
Rick Copeland <rick@arborian.com>
2021-10-07 21:58:30 +0000

    Begin thinking about unit of work
    - move from dirty flag to new/clean/dirty/deleted status

--- barin2/identity_map.py
+++ barin2/identity_map.py
@@ -5,10 +5,10 @@ class IdentityMap:
     def __init__(self):
         self._idmap = {}
 
-    def process(self, collection, doc):
+    def process(self, collection, doc, status):
         """Process and insert a single 'raw' doc from MongoDB"""
         key = (collection, doc["_id"])
-        result = self._idmap[key] = instrument_object(doc)
+        result = self._idmap[key] = instrument_object(doc, status)
         return result
 
     def get(self, collection, _id, default=None):
--- barin2/instrumentation.py
+++ barin2/instrumentation.py
@@ -1,3 +1,4 @@
+import enum
 from functools import singledispatch
 
 STATE_MAGIC = "__b2_state__"
@@ -8,6 +9,13 @@ MUTATING_LIST_METHODS = """
     __setitem__ __delitem__ append clear extend insert pop remove
     reverse
     """.split()
+
+
+class Status(enum.Enum):
+    NEW = enum.auto()
+    CLEAN = enum.auto()
+    DIRTY = enum.auto()
+    DELETED = enum.auto()
 
 
 def init_module():
@@ -23,20 +31,29 @@ def get_state(obj):
 class InstrumentedState:
     def __init__(self):
         self.container = None
-        self._dirty = False
+        self._status = Status.CLEAN
 
     def __repr__(self):
-        return f"<istate dirty={self._dirty} container={self.container}>"
+        return f"<istate status={self._status} container={self.container}>"
 
     @property
-    def dirty(self):
-        return self._dirty
+    def status(self):
+        return self._status
 
-    @dirty.setter
-    def dirty(self, value):
-        self._dirty = value
-        if value and self.container:
-            get_state(self.container).dirty = True
+    @status.setter
+    def status(self, value):
+        old_status = self._status
+        self._status = value
+        # If we move from CLEAN => DIRTY, dirty up all our containers
+        if self.container and (old_status, value) == (
+            Status.CLEAN,
+            Status.DIRTY,
+        ):
+            get_state(self.container).status = value
+
+    def on_modify(self):
+        if self._status == Status.CLEAN:
+            self.status = Status.DIRTY
 
 
 class Instrumented:
@@ -67,27 +84,29 @@ def instrument_method(cls, method_name):
     super_method = getattr(cls, method_name)
 
     def f(self, *args, **kwargs):
-        get_state(self).dirty = True
+        get_state(self).on_modify()
         return super_method(self, *args, **kwargs)
 
     return f
 
 
 @singledispatch
-def instrument_object(obj, container=None):
+def instrument_object(obj, status, container=None):
     return obj
 
 
 @instrument_object.register
-def _instrument_object_dict(obj: dict, container=None):
-    obj = Idict((key, instrument_object(value)) for key, value in obj.items())
+def _instrument_object_dict(obj: dict, status, container=None):
+    obj = Idict(
+        (key, instrument_object(value, status)) for key, value in obj.items()
+    )
     instrument_object_container(obj, container)
     return obj
 
 
 @instrument_object.register
-def _instrument_object_list(obj: list, container=None):
-    obj = Ilist(instrument_object(value) for value in obj)
+def _instrument_object_list(obj: list, status, container=None):
+    obj = Ilist(instrument_object(value, status) for value in obj)
     instrument_object_container(obj, container)
     return obj
 
@@ -112,22 +131,22 @@ def _instrument_object_container_list(obj: list, container):
 
 
 @singledispatch
-def cleanse_object(obj):
+def set_object_status(obj, status):
     pass
 
 
-@cleanse_object.register
-def _cleanse_object_dict(obj: dict):
-    get_state(obj).dirty = False
+@set_object_status.register
+def _cleanse_object_dict(obj: dict, status):
+    get_state(obj).status = status
     for value in obj.values():
-        cleanse_object(value)
+        set_object_status(value, status)
 
 
-@cleanse_object.register
-def _cleanse_object_list(obj: list):
-    get_state(obj).dirty = False
+@set_object_status.register
+def _cleanse_object_list(obj: list, status):
+    get_state(obj).status = status
     for value in obj:
-        cleanse_object(value)
+        set_object_status(value, status)
 
 
 init_module()
